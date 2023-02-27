f6c02465501ef1f505b09add7dd6a05c4627e8f4
Leonardo Harley <leoharleygoncalves@gmail.com>
2023-02-27 09:31:48 -0300

    ...

--- sistema/application/views/admin/super/welcome.php
+++ sistema/application/views/admin/super/welcome.php
@@ -29,7 +29,7 @@ echo viewHelper::getViewTestTag('index');
 
     <!-- Logo & Presentation -->
     <?php if ($bShowLogo) :?>
-        <div class="row">
+        <div class="row" style="margin-right: 40px; margin-left: 40px">
             <div class="jumbotron" id="welcome-jumbotron">
                 <img alt="logo" src="<?php echo LOGO_URL;?>" id="lime-logo"  class="profile-img-card img-responsive center-block" />
                <!-- <p class="hidden-xs custom custom-margin top-25" ><?php //echo PRESENTATION; // Defined in AdminController?></p> -->
